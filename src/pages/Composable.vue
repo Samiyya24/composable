b497576381749baa5c9d5866d26d227ff056631e
Samiyya24 <samiyyaazizkhanova904@gmail.com>
2024-06-11 08:32:22 +0500

    first commit

---
+++ src/pages/Composable.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container flex flex-col items-center gap-5 my-20">
+    <h1 class="text-red-500 text-5xl">Composable</h1>
+    <div>
+      <h2 class="text-green-500 text-5xl">Products</h2>
+      <div class="grid grid-cols-4 gap-5">
+        <div
+          class="bg-gray-100/50 p-5 rounded"
+          v-for="product in products"
+          :key="product.id"
+        >
+          <img :src="product.thumbnail" alt="" />
+
+          <h1 class="text-2xl font-medium line-clamp-1 my-3">
+            {{ product.title }}
+          </h1>
+          <p class="line-clamp-2 mb-3">{{ product.description }}</p>
+          <h1 class="text-2xl font-medium">{{ product.price }} $</h1>
+        </div>
+        </div>
+        </div>
+      <Loading v-if="loading" />
+    <button @click="test" class="bg-blue-500 p-2 text-white rounded-xl">leadMore</button>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, watch } from "vue";
+import Loading from "../components/Loading.vue";
+import { useProducts } from "../composable/useProducts";
+// import { useInterSectionObServer } from "../composable/useIntersectionObserver";
+
+const loadmore = ref(null);
+// const { isInterSecting } = useInterSectionObServer(loadmore);
+const { products, error, loading, fetchProducts } = useProducts();
+
+// watch(isInterSecting, (newValue) => {
+//   if (newValue) {
+//     fetchProducts();
+//   }
+// });
+
+fetchProducts();
+const test = () => {
+  fetchProducts();
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/pages/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="py-10">
+    <div class="container">
+
+      <h1 class="mb-10">Home page</h1>
+
+      <OurProjects />
+      <Composable/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Navbar from '../components/Navbar.vue';
+import OurProjects from '../components/OurProjects.vue';
+import Composable from './Composable.vue';
+
+
+
+</script>
+
+<style lang="scss" scoped></style>
